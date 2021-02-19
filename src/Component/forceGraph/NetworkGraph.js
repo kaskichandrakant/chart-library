f603d8c70bf78bdc36ef5b0b4d8517af7cd8da1c
Chandrakant Kaski <kaskichandrakant@gmail.com>
2021-02-19 22:49:43 +0530

    removing unneccesary dependencies

--- src/Component/forceGraph/NetworkGraph.js
+++ src/Component/forceGraph/NetworkGraph.js
@@ -1,22 +1,23 @@
-// import React, {useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 // import {ForceGraph2D} from 'react-force-graph';
-// import createGraphData from '../../utils/graph';
-//
-// const NetworkGraph = () => {
-//   const ref = useRef();
-//   useEffect(() => {
-//     ref.current.d3Force('charge').strength(-100);
-//   });
-//   return (
-//     <ForceGraph2D
-//       ref={ref}
-//       graphData={createGraphData(10, 100)}
-//       height={500}
-//       d3Force={(...a) => {
-//         console.log(a);
-//       }}
-//     />
-//   );
-// };
-//
-// export default NetworkGraph;
+import createGraphData from '../../utils/graph';
+
+const NetworkGraph = () => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current.d3Force('charge').strength(-100);
+  });
+  return (
+    <div></div>
+    // <ForceGraph2D
+    //   ref={ref}
+    //   graphData={createGraphData(10, 100)}
+    //   height={500}
+    //   d3Force={(...a) => {
+    //     console.log(a);
+    //   }}
+    // />
+  );
+};
+
+export default NetworkGraph;
--- src/Component/forceGraph/NetworkGraph2.js
+++ src/Component/forceGraph/NetworkGraph2.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import {ForceGraph, ForceGraphNode, ForceGraphLink} from 'react-vis-force';
+// import {ForceGraph, ForceGraphNode, ForceGraphLink} from 'react-vis-force';
 import createGraphData from "../../utils/graph";
 
 const graph = createGraphData(10,10);
 
 const NetworkGraph2 = () => {
-  return (
-    <ForceGraph simulationOptions={{ height: 300, width: 300 }}>
-      {graph.nodes.map((node)=><ForceGraphNode node={{ id: node.id }} fill="red" />)}
-      {graph.links.slice(1,100).map((link)=> <ForceGraphLink link={{ source: link.source, target: link.target }} />)}
-    </ForceGraph>
+  return (<div></div>
+    // <ForceGraph simulationOptions={{ height: 300, width: 300 }}>
+    //   {graph.nodes.map((node)=><ForceGraphNode node={{ id: node.id }} fill="red" />)}
+    //   {graph.links.slice(1,100).map((link)=> <ForceGraphLink link={{ source: link.source, target: link.target }} />)}
+    // </ForceGraph>
   )
 }
 
